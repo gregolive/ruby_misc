b28701074630780106cdf32c9ef655921ade16b6
Gregory Olive <gregoryolive@outlook.com>
2021-09-30 02:12:51 +0900

    Added substrings method

---
+++ substrings.rb
@@ -0,0 +1,15 @@
+# takes a string and lists the number of times each word in the string is found in a dictionary array
+
+def substrings(string, dictionary)
+  p (dictionary.reduce(Hash.new(0)) { |result, word|
+    match = string.downcase.scan(/#{word}/)
+    if match.any? then
+      result[word] = match.length
+    end
+    result
+  })
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit", "be"]
+substrings("below below it", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
