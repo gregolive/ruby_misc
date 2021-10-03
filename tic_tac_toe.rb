e0314c905205b11ad300783036e3909b27f97f98
Gregory Olive <gregoryolive@outlook.com>
2021-10-03 22:28:54 +0900

    Updated tic_tac_toe.rb

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -3,37 +3,32 @@
 # Can win vertically, horizontally or diagonally
 module WinConditions
   def all_same?(array)
-    array.all? {|e| e == array[0]}
+    array.all? { |e| e == array[0] }
   end
 
-  def horizontal_win(a)
-    if all_same?(a[0..2]) || all_same?(a[3..5]) || all_same?(a[6..8])
-      return true
-    end
+  def horizontal_win(arr)
+    return true if all_same?(arr[0..2]) || all_same?(arr[3..5]) || all_same?(arr[6..8])
   end
 
-  def vertical_win(a)
-    column1 = [a[0], a[3], a[6]]
-    column2 = [a[1], a[4], a[7]]
-    column3 = [a[2], a[5], a[8]]
-    if all_same?(column1) || all_same?(column2) || all_same?(column3)
-      return true
-    end
+  def vertical_win(arr)
+    column1 = [arr[0], arr[3], arr[6]]
+    column2 = [arr[1], arr[4], arr[7]]
+    column3 = [arr[2], arr[5], arr[8]]
+    return true if all_same?(column1) || all_same?(column2) || all_same?(column3)
   end
 
-  def diagonal_win(a)
-    diagonal1 = [a[0], a[4], a[8]]
-    diagonal2 = [a[2], a[4], a[6]]
-    if all_same?(diagonal1) || all_same?(diagonal2)
-      return true
-    end
+  def diagonal_win(arr)
+    diagonal1 = [arr[0], arr[4], arr[8]]
+    diagonal2 = [arr[2], arr[4], arr[6]]
+    return true if all_same?(diagonal1) || all_same?(diagonal2)
   end
 end
 
+# Player Superclass
 class Player
   include WinConditions
   attr_reader :name, :marker
-  
+
   @@round_number = 0
   @@board = "1 | 2 | 3\n―――――――――\n4 | 5 | 6\n―――――――――\n7 | 8 | 9"
   @@board_list = [1, 2, 3, 4, 5, 6, 7, 8, 9] # so that users don't replace board lines with their move
@@ -70,24 +65,31 @@ class Player
   end
 
   def check_winner
-    if horizontal_win(@@board_list) || vertical_win(@@board_list) || diagonal_win(@@board_list)
-      puts "\n――――――――――――――――――――― GAME OVER ―――――――――――――――――――――"
-      puts "\nCONGRATS! #{@name} is the winner!\n\n"
-      @@winner = !@@winner
-    end
+    return unless horizontal_win(@@board_list) || vertical_win(@@board_list) || diagonal_win(@@board_list)
+
+    puts "\n――――――――――――――――――――― GAME OVER ―――――――――――――――――――――"
+    puts "\nCONGRATS! #{@name} is the winner!\n\n"
+    @@winner = !@@winner
   end
-  
+
+  def self.game_reset
+    @@board = "1 | 2 | 3\n―――――――――\n4 | 5 | 6\n―――――――――\n7 | 8 | 9"
+    @@board_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    @@round_number = 0
+    @@winner = false
+  end
+
   # Protected instance methods
   protected
 
   # for setup...
   def ask_for_name
-    print "Enter your name: "
+    print 'Enter your name: '
     @name = gets.chomp
   end
-  
+
   def ask_for_marker
-    print "Type one non-number character for your game marker: "
+    print 'Type one non-number character for your game marker: '
     @marker = gets.chomp
 
     marker_length_check
@@ -98,16 +100,16 @@ class Player
     # only allow one character
     while @marker.length > 1
       print "ERROR: #{@marker} is longer than 1 character.\n"
-      print "Type one non-number character for your game marker: "
+      print 'Type one non-number character for your game marker: '
       @marker = gets.chomp
     end
   end
 
   def marker_number_check
     # don't allow numbers
-    while @@board.include?(@marker) || @marker == 0
+    while @@board.include?(@marker) || @marker == '0'
       print "ERROR: #{@marker} is a number.\n"
-      print "Type one non-number character for your game marker: "
+      print 'Type one non-number character for your game marker: '
       @marker = gets.chomp
     end
   end
@@ -120,36 +122,30 @@ class Player
   def change_board
     number = gets.chomp
     while @@board_list.include?(number.to_i) == false
-      print "ERROR: You must choose a number from the board."
+      print 'ERROR: You must choose a number from the board.'
       ask_for_move
       number = gets.chomp
     end
     sub_number(number)
   end
 
-  def self.game_reset
-    @@board = "1 | 2 | 3\n―――――――――\n4 | 5 | 6\n―――――――――\n7 | 8 | 9"
-    @@board_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @@round_number = 0
-    @@winner = false
-  end
-
   def sub_number(number)
-    @@board_list.map! { |e| 
-      e == number.to_i ? e = @marker : e
-    }
+    @@board_list.map! do |e|
+      e == number.to_i ? @marker : e
+    end
     @@board.sub! number, @marker
   end
-
 end
 
+# Player 1 Class
 class Player1 < Player
   def setup
-    puts "PLAYER 1"
+    puts 'PLAYER 1'
     ask_questions
   end
 end
 
+# Player 2 Class
 class Player2 < Player
   def setup
     puts "\nPLAYER 2"
@@ -159,24 +155,13 @@ end
 
 # Opening and player set up
 
-# retro style opening
-=begin
-print "Initializing"
-sleep(0.8)
-print " ."
-sleep(0.8)
-print " ."
-sleep(0.8)
-print " ."
-sleep(0.8)
-=end
 puts "\n\n――――――――――――――――― RETRO TIC TAC TOE ―――――――――――――――――\n\n"
 
 # set up the game
-player1 = Player1.new()
+player1 = Player1.new
 player1.setup
 
-player2 = Player2.new()
+player2 = Player2.new
 player2.setup
 # player 2 can't use the same marker as player 1
 while player2.marker == player1.marker
@@ -187,8 +172,8 @@ end
 # Play tic tac toe
 play = true
 while play == true
-  random_start = [true, false] #random player goes first
-  player_one_turn = random_start[rand(2) - 1]
+  random_start = [true, false] # random player goes first
+  player_one_turn = random_start[rand(-1..0)]
 
   while Player.round_number < 9 && Player.winner? == false
     if player_one_turn == true
@@ -207,15 +192,15 @@ while play == true
   print "\n\nWould you like to play again? (Y/N)\n"
   again_response = gets.chomp
 
-  while !(again_response == "Y" || again_response == "N")
+  until %w[Y N].include?(again_response)
     puts "ERROR: Type 'Y' for YES and 'N' for NO."
     print "\nWould you like to play again? (Y/N)\n"
     again_response = gets.chomp
   end
 
-  if again_response == "N"
-     play = false
-     puts "Thank you for playing!"
+  if again_response == 'N'
+    play = false
+    puts "\nThanks for playing!\n"
   else
     Player.game_reset
   end
