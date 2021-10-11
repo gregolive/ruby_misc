e0b669c2bb0797046c0c93cde7fec576425a79e1
Gregory Olive <gregoryolive@outlook.com>
2021-10-11 23:14:42 +0900

    Finished up to my_any?

--- custom_enumerables.rb
+++ custom_enumerables.rb
@@ -4,18 +4,78 @@ module Enumerable
   # EACH
   def my_each
     index = 0
-    while index < self.length
-      yield self[index]
+    output = Array.new()
+    while index < self.size
+      yield self.to_a[index]
+      output.push(self.to_a[index])
       index += 1
     end
+    output
   end
 
   # EACH WITH INDEX
   def my_each_with_index
     index = 0
-    while index < self.length
-      yield self[index], index
+    output = Array.new()
+    while index < self.size
+      yield self.to_a[index], index
+      output.push(self.to_a[index])
       index += 1
     end
+    output
+  end
+
+  # SELECT
+  def my_select
+    output = Array.new()
+    self.my_each do |element|
+      output.push(element) if yield(element)
+    end
+    output
+  end
+
+  # ALL?
+  def my_all?(arg = "")
+    output = Array.new()
+    if block_given?
+      self.my_each do |element|
+        output.push(element) if yield(element)
+      end  
+    else
+      if arg == ""
+        self.my_each do |element|
+          output.push(element) if element
+        end
+      else
+        self.my_each do |element|
+          arg.is_a?(Regexp) ? (output.push(element) if element.match?(arg)) : (output.push(element) if element.is_a?(arg))
+        end
+      end
+    end
+    output == self ? true : false
+  end
+  
+  # ANY?
+  def my_any?(arg = "")
+    if block_given?
+      self.my_each do |element|
+        return true if yield(element)
+      end  
+    else
+      if arg == ""
+        self.my_each do |element|
+          return true if element
+        end
+      else
+        self.my_each do |element|
+          if arg.is_a?(Regexp) 
+            return true if element.match?(arg)
+          else 
+            return true if element.is_a?(arg)
+          end
+        end
+      end
+    end
+    false
   end
 end
