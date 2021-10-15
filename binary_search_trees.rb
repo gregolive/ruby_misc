c6a93b6558f8d41f2b2327e162cfad549d4fb233
Gregory Olive <gregoryolive@outlook.com>
2021-10-15 23:57:17 +0900

    Added insert function

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -4,41 +4,56 @@
 class Node
   include Comparable
 
-  attr_accessor :left_child, :right_child
+  attr_accessor :left, :right
   attr_reader :data
 
   def initialize(data)
     @data = data
-    @left_child = nil
-    @right_child = nil
+    @left = @right = nil
   end
 end
 
 class Tree
-  attr_reader :array
+  attr_reader :array, :size
 
   def initialize(array)
-    @array = array.uniq
+    @array = array.uniq.sort
+    @size = array.length
     @root = build_tree
   end
 
-  def build_tree(array = @array, start = 0, finish = array.length - 1)
+  def build_tree(array = @array, start = 0, finish = @size - 1)
     return if start > finish
 
     mid = (start + finish) / 2
     root = Node.new(array[mid])
-    root.left_child = build_tree(array, start, mid-1)
-    root.right_child = build_tree(array, mid+1, finish)
+    root.left = build_tree(array, start, mid-1)
+    root.right = build_tree(array, mid+1, finish)
     return root
   end
 
   def draw_tree(node = @root, prefix = '', is_left = true)
-    draw_tree(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
+    draw_tree(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
-    draw_tree(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
+    draw_tree(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def insert(data, cur = @root, prev = nil)
+    if cur.nil?
+      @size += 1
+      return data < prev.data ? prev.left = Node.new(data) : prev.right = Node.new(data)
+    end
+
+    if data < cur.data
+      insert(data, cur.left, cur)
+    else
+      insert(data, cur.right, cur)
+    end
   end
 end
 
-my_tree = Tree.new([1,2,3,4,5,6,7,8,9])
-my_tree.build_tree
+my_tree = Tree.new([1,3,4,6,7,8,9])
+puts my_tree.draw_tree
+
+p my_tree.insert(2)
 puts my_tree.draw_tree
